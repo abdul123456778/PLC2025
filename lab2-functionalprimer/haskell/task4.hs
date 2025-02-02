3083131cf4357e11cf7f4fe57ad9373ff59ce99a
Abdelrahman tabash <tabashabdelrahman@gmail.com>
2025-02-02 19:58:04 +0000

    Edited task1-4 GitPod

--- lab2-functionalprimer/haskell/task4.hs
+++ lab2-functionalprimer/haskell/task4.hs
@@ -1,14 +1,17 @@
-ask :: String -> IO ()
-ask prompt =
-  do
-  putStrLn prompt
+ask :: String -> Int -> IO ()
+ask prompt exclamations = do
+  putStrLn (prompt ++ replicate exclamations '!')
   line <- getLine
-  if line == ""
-    then ask prompt
-    else putStrLn ("you said: " ++ reverse line)
+  if line == "quit"
+    then putStrLn "quitting..."
+    else if line == ""
+    then ask prompt (exclamations + 1)
+    else do
+        putStrLn ("you said: " ++ reverse line)
+        ask prompt 0  -- Reset "!" count after valid input
+
 
 main :: IO ()
-main =
-  do
+main = do
   let prompt = "please say something"
-  ask prompt
+  ask prompt 0
