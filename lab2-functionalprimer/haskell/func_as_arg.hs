84f702749ba012ba1c9a5f73693bb0610c93cdaa
Abdelrahman tabash <tabashabdelrahman@gmail.com>
2025-02-03 15:49:28 +0000

    modified func_as_arg in gitpod

--- lab2-functionalprimer/haskell/func_as_arg.hs
+++ lab2-functionalprimer/haskell/func_as_arg.hs
@@ -1,9 +1,9 @@
 --create inpFunc
-inpFunc = [1..5] 
+inpFunc a b = [a..b] 
 
 --Define applicatorFunc
-applicatorFunc inpFunc s = if s=='s' then sum inpFunc else (sum inpFunc)/5  
+applicatorFunc inpFunc s = if s=='s' then sum inpFunc else (fromIntegral (sum inpFunc)) / (fromIntegral (length inpFunc))
 
 main = do
-    let result = applicatorFunc inpFunc 'a' --Call applicatorFunc with inpFunc and 'a' as args
+    let result = applicatorFunc (inpFunc a b) 'a' --Call applicatorFunc with inpFunc and 'a' as args
     putStrLn("sum = " ++ show(result))
