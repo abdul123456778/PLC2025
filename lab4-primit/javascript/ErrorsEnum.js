3d2a0716f0c34aae8d22d81dd10a3a96a15780f4
Abdelrahman tabash <tabashabdelrahman@gmail.com>
2025-02-19 13:03:07 +0000

    javascript

--- lab4-primit/javascript/ErrorsEnum.js
+++ lab4-primit/javascript/ErrorsEnum.js
@@ -1,49 +1,49 @@
 const prompt=require("prompt-sync")({sigint:true}); 
 
 const Error_enumobj = {
-	FP_ROUNDING: "FP_ROUNDING",
-	FP_OVERFLOW: "FP_OVERFLOW",
-	FP_UNDERFLOW: "FP_UNDERFLOW",
-	INT_OVERFLOW: "INT_OVERFLOW"
+	FP_ROUNDING:"FP_ROUNDING",
+	FP_OVERFLOW:"FP_OVERFLOW",
+	FP_UNDERFLOW:"FP_UNDERFLOW",
+	INT_OVERFLOW:"INT_OVERFLOW"
 }
 
 const Result_enumobj = {
-    A_BIT_DIFFERENT: "A_BIT_DIFFERENT", 
-    INFINITY: "INFINITY", 
-    ZERO: "ZERO", 
-    VERY_DIFFERENT: "VERY_DIFFERENT"
+    A_BIT_DIFFERENT:"A_BIT_DIFFERENT", 
+    INFINITY:"INFINITY", 
+    ZERO:"ZERO", 
+    VERY_DIFFERENT:"VERY_DIFFERENT"
 }
 
-function error2Result(err){
-    switch (err) {
-	case Error_enumobj.FP_ROUNDING:
-	 return Result_enumobj.A_BIT_DIFFERENT;
-	break;
-	case Error_enumobj.FP_OVERFLOW:
-	    return Result_enumobj.INFINITY;
-	break;
-	case Error_enumobj.FP_UNDERFLOW:
-	    return Result_enumobj.ZERO;
-	break;
-	case Error_enumobj.INT_OVERFLOW:
-	    return Result_enumobj.VERY_DIFFERENT;
-	break;
-	default:
-		return 'Invalid Error value';
+function result2Error(result){
+    switch (result) {
+	case Result_enumobj.A_BIT_DIFFERENT:
+     return Error_enumobj.FP_ROUNDING;
+    break;
+    case Result_enumobj.INFINITY:
+     return Error_enumobj.FP_OVERFLOW;
+    break;
+    case Result_enumobj.ZERO:
+     return Error_enumobj.FP_UNDERFLOW;
+    break;
+    case Result_enumobj.VERY_DIFFERENT:
+     return Error_enumobj.INT_OVERFLOW;
+    break;
+    default:
+     return 'Invalid Result value';
 }
 
 }
 
-console.log('Error list: ', Object.values(Error_enumobj));
+console.log('Result list: ', Object.values(Result_enumobj));
 var validArg = false;
 while(!validArg){
-    var input = prompt("Input: ");
-    let result = error2Result(input);
-    if (Object.values(Result_enumobj).includes(result)){
+    var input = prompt("Input: ").toUpperCase().trim();
+    let error = result2Error(input);
+    if (Object.values(Error_enumobj).includes(error)){
         validArg = true;
-		console.log(input + " results in " + error2Result(input));
+		console.log(input + " is cause by " + error);
     }
     else{
-        console.log(result);
+        console.log(error);
     }
 }
